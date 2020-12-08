0926d9ce2c2991d847e3ed1d6b1913e67fac1b69
Maranda L <marandalevine@gmail.com>
2020-12-08 22:44:34 +0000

    list and dict challenge

---
+++ list_and_dict_challenge.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+dog_names = ["cain", "trixie", "mike"]
+print(dog_names)
+
+cat_names = ["king", "kitty", "niche"]
+print(cat_names)
+
+dog_names.extend["kingKittyniche"]
+print(dog_names)
+
+dogs.append(cats)
+
